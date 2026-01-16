c919582e493814038f139c7f9ac55f44dc45b7ac
biel11dev <gabrielb.ribeiro14@gmail.com>
2026-01-16 18:06:41 -0300

    correção end api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3001/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://api-start-pira-net.vercel.app/';
 
 const api = axios.create({
   baseURL: API_URL,
